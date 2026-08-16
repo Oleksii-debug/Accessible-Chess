ea493cf573a2cc42ffdd81794fd40714459df69b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-16 17:55:04 +0300

    WordDeck: add app entry point

---
+++ WordDeck/Program.cs
@@ -0,0 +1,11 @@
+namespace WordDeck;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
