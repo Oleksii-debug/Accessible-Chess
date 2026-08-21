f56eeded1679784766fccc03b095565e5de9a77e
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-21 13:40:11 +0300

    WordDeck: harden human-readable shortcut formatting

--- WordDeck/ShortcutFormatter.cs
+++ WordDeck/ShortcutFormatter.cs
@@ -13,22 +13,56 @@ internal static class ShortcutFormatter
         if (modifiers.HasFlag(Keys.Alt)) parts.Add("Alt");
 
         Keys code = keys & Keys.KeyCode;
-        if (code != Keys.None)
-        {
-            int codeValue = (int)code;
-            string key = code switch
-            {
-                >= Keys.D0 and <= Keys.D9 => ((char)('0' + codeValue - (int)Keys.D0)).ToString(),
-                >= Keys.NumPad0 and <= Keys.NumPad9 => "Num" + (codeValue - (int)Keys.NumPad0),
-                Keys.Oemcomma => ",",
-                Keys.OemPeriod => ".",
-                Keys.OemMinus => "-",
-                Keys.Oemplus => "+",
-                _ => code.ToString()
-            };
-            parts.Add(key);
-        }
+        if (code != Keys.None) parts.Add(FormatKeyCode(code));
 
-        return parts.Count == 0 ? keys.ToString() : string.Join("+", parts);
+        return parts.Count == 0 ? "Unassigned" : string.Join("+", parts);
+    }
+
+    private static string FormatKeyCode(Keys code)
+    {
+        int value = (int)code;
+        if (code is >= Keys.D0 and <= Keys.D9)
+            return ((char)('0' + value - (int)Keys.D0)).ToString();
+        if (code is >= Keys.A and <= Keys.Z)
+            return ((char)('A' + value - (int)Keys.A)).ToString();
+        if (code is >= Keys.NumPad0 and <= Keys.NumPad9)
+            return "Num" + (value - (int)Keys.NumPad0);
+        if (code is >= Keys.F1 and <= Keys.F24)
+            return "F" + (value - (int)Keys.F1 + 1);
+
+        return code switch
+        {
+            Keys.Up => "Up",
+            Keys.Down => "Down",
+            Keys.Left => "Left",
+            Keys.Right => "Right",
+            Keys.PageUp => "Page Up",
+            Keys.PageDown => "Page Down",
+            Keys.Home => "Home",
+            Keys.End => "End",
+            Keys.Insert => "Insert",
+            Keys.Delete => "Delete",
+            Keys.Back => "Backspace",
+            Keys.Space => "Space",
+            Keys.Enter => "Enter",
+            Keys.Escape => "Escape",
+            Keys.Tab => "Tab",
+            Keys.Oemcomma => ",",
+            Keys.OemPeriod => ".",
+            Keys.OemMinus => "-",
+            Keys.Oemplus => "+",
+            Keys.OemQuestion => "/",
+            Keys.OemSemicolon => ";",
+            Keys.OemQuotes => "'",
+            Keys.OemOpenBrackets => "[",
+            Keys.OemCloseBrackets => "]",
+            Keys.OemPipe => "\\",
+            Keys.Add => "Num+",
+            Keys.Subtract => "Num-",
+            Keys.Multiply => "Num*",
+            Keys.Divide => "Num/",
+            Keys.Decimal => "Num.",
+            _ => Enum.GetName(typeof(Keys), code) ?? $"Key {value}"
+        };
     }
 }
