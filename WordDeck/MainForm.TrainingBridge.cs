82b5e65423797d6a0d373a17067ac0a0a296c3af
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-21 18:15:18 +0300

    WordDeck R2: retain selector focus during keyboard changes

--- WordDeck/MainForm.TrainingBridge.cs
+++ WordDeck/MainForm.TrainingBridge.cs
@@ -2,6 +2,8 @@ namespace WordDeck;
 
 internal sealed partial class MainForm
 {
+    private ComboBox? _keyboardSelectorPendingFocus;
+
     internal AppState SharedAppStateForTraining => _state;
 
     internal DictionaryPackage ActivePackageForTraining => _package;
@@ -9,5 +11,36 @@ internal sealed partial class MainForm
     internal void SaveSharedStateAfterTraining()
     {
         SaveState();
+    }
+
+    internal void InstallReleaseSelectorFocusRetention()
+    {
+        InstallSelectorFocusRetention(_dictionaryCombo);
+        InstallSelectorFocusRetention(_scopeCombo);
+        InstallSelectorFocusRetention(_deckCombo);
+    }
+
+    private void InstallSelectorFocusRetention(ComboBox combo)
+    {
+        combo.KeyDown += (_, e) =>
+        {
+            if (e.Modifiers == Keys.None && (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down))
+                _keyboardSelectorPendingFocus = combo;
+        };
+        combo.SelectedIndexChanged += (_, _) =>
+        {
+            if (!ReferenceEquals(_keyboardSelectorPendingFocus, combo)) return;
+            _keyboardSelectorPendingFocus = null;
+            BeginInvoke(new Action(() =>
+            {
+                if (!IsDisposed && combo.CanFocus)
+                    combo.Focus();
+            }));
+        };
+        combo.Leave += (_, _) =>
+        {
+            if (ReferenceEquals(_keyboardSelectorPendingFocus, combo))
+                _keyboardSelectorPendingFocus = null;
+        };
     }
 }
