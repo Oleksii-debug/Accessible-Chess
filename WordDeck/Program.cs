111fe1930e14ee2d17d5b99a8af062104fa8a88b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-23 18:21:53 +0300

    WordDeck DEV08: include adaptive router in complete self-test

--- WordDeck/Program.cs
+++ WordDeck/Program.cs
@@ -22,6 +22,7 @@ internal static class Program
                 SentenceCoachSelfTest.Run();
                 SentencePackStoreSelfTest.Run();
                 TatoebaSentencePackSelfTest.Run();
+                AdaptiveMasteryRouterSelfTest.Run();
             }
             catch (Exception ex)
             {
